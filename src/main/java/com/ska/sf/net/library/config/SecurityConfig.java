715dbafd381c6fc115b5ba3faff19480929b8547
avinashkumark <30576197+avinashkumark@users.noreply.github.com>
2018-04-02 23:52:57 -0400

    impr 1
    ipr 1

--- src/main/java/com/ska/sf/net/library/config/SecurityConfig.java
+++ src/main/java/com/ska/sf/net/library/config/SecurityConfig.java
@@ -13,19 +13,32 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*@Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-*/
+    //@Resource(name = "userDetailsService()")
+    //private UserDetailsService userDetailsService;
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        return encoder;
+//    }
+
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
 
         InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        /*inMemoryUserDetailsManager.createUser(User.builder().username("avinash").password(new BCryptPasswordEncoder().encode("password")).roles("USER").build());
+        inMemoryUserDetailsManager.createUser(User.builder().username("varsha").password(new BCryptPasswordEncoder().encode("password")).roles("ADMIN").build());*/
         inMemoryUserDetailsManager.createUser(User.withDefaultPasswordEncoder().username("avinash").password("password").roles("USER").build());
         inMemoryUserDetailsManager.createUser(User.withDefaultPasswordEncoder().username("varsha").password("password").roles("ADMIN").build());
-
+        //userDetailsService = inMemoryUserDetailsManager;
         return inMemoryUserDetailsManager;
     }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
